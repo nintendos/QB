c579f713ddd73d4178b75fa905988fc65c618c9e
nintendos <sodnetnin@gmail.com>
2015-09-13 18:58:25 +0800

    d

--- app.js
+++ app.js
@@ -1,32 +1,3 @@
-/**
-* Module dependencies.
-*/
-// var express = require('express'), routes = require('./routes');
-// var app = module.exports = express.createServer();
-
-// // Configuration
-// app.configure(function(){
-// 	app.set('views', __dirname + '/views');
-// 	app.set('view engine', 'ejs');
-// 	app.use(express.bodyParser());
-// 	app.use(express.methodOverride());
-// 	app.use(app.router);
-// 	app.use(express.static(__dirname + '/public'));
-// });
-// app.configure('development', function(){
-// 	app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-// });
-// app.configure('production', function(){
-// 	app.use(express.errorHandler());
-// });
-// // Routes
-// app.get('/', routes.index);
-// app.listen(3000);
-// console.log("Express server listening on port %d in %s mode", app.address().port,
-// app.settings.env);
-
-
-
 //////////////////
 var express = require('express');
 var path = require('path');
@@ -45,7 +16,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -57,9 +28,9 @@ app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handlers
@@ -67,24 +38,27 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
     });
+  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
 
 
 module.exports = app;
+
+
+//////////////
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { pagetitle: '我的主页 - Quoteboard' });
 });
+
 module.exports = router;
